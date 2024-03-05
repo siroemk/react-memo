311b3d4dfe3184378ef1a9109183e82f3a4093f7
emiko <31835314+siroemk@users.noreply.github.com>
2024-03-05 17:35:53 +0900

    メモの追加、編集、削除を実装した

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
 import './App.css';
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+  const [memos, setMemos] = useState([]);
+  const [selectedMemoId, setSelectedMemoId] = useState(null);
+  const [memoContent, setMemoContent] = useState('');
+
+  function handleMemoAdd() {
+    const newMemos = [...memos, { id: uuidv4(), content: '新規メモ' }];
+    setSelectedMemoId(newMemos[newMemos.length - 1].id);
+    setMemoContent(newMemos[newMemos.length - 1].content);
+    setMemos(newMemos);
+  }
+
+  function handleMemoEdit(id, content) {
+    const newMemos = [...memos];
+    const updateMemo = newMemos.find((memo) => memo.id === id);
+    updateMemo.content = content;
+    setMemos(newMemos);
+  }
+
+  function handleMemoDelete(id) {
+    const newMemos = [...memos];
+    const updateMemos = newMemos.filter((memo) => memo.id !== id)
+    setMemos(updateMemos);
+    setSelectedMemoId(null);
+  }
+
+  function handleMemoSelect(id) {
+    const selectedMemo = memos.find((memo) => memo.id === id);
+    setMemoContent(selectedMemo.content);
+    setSelectedMemoId(id);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>   
+      <ul>
+        {memos.map((memo) => (
+          <li key={memo.id} onClick={() => handleMemoSelect(memo.id)}>{memo.content}</li>
+        ))}
+      </ul>
+      <button onClick={handleMemoAdd}>+</button>
+
+      { selectedMemoId !== null&&(
+        <>
+          <textarea value={memoContent} onChange={(e) => setMemoContent(e.target.value)}  />
+          <div>
+            <button onClick={() => handleMemoEdit(selectedMemoId, memoContent)}>編集</button>
+            <button onClick={() => handleMemoDelete(selectedMemoId)}>削除</button>
+          </div>
+        </>
+        )
+      }
+    </> 
   );
 }
 
